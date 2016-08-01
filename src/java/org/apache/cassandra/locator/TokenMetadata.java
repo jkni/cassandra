e5408177d9db44ebad85aeb2fa625ca3b00543b7
Joel Knighton <joel.knighton@datastax.com>
2016-08-01 17:04:57 -0500

    Add collections4 dep

--- src/java/org/apache/cassandra/locator/TokenMetadata.java
+++ src/java/org/apache/cassandra/locator/TokenMetadata.java
@@ -27,7 +27,7 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.google.common.collect.*;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
